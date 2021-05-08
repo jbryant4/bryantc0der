bdb49bb2f4d2d3d4ab610b5426a914add7f6ae19
JoKing <joebryant614@gmail.com>
2021-05-08 14:05:54 -0700

    almost done

--- assets/js/main.js
+++ assets/js/main.js
@@ -143,3 +143,12 @@
 			});
 
 })(jQuery);
+
+//!send email to me using message button 
+
+// curl --request POST \
+// 	--url https://api.sendgrid.com/v3/mail/send \
+// 	--header "Authorization: Bearer $SENDGRID_API_KEY" \
+// 	--header 'Content-Type: application/json' \
+// 	--data '{"personalizations": [{"to": [{"email": "test@example.com"}]}],"from": {"email": "test@example.com"},"subject": "Sending with SendGrid is Fun","content": [{"type": "text/plain", "value": "and easy to do anywhere, even with cURL"}]}'
+
